ce0a7c1c24a4b4be28e0d89043baef986b704da2
Ghazalehz <ghazalehz@physik.fu-berlin.de>
2019-06-22 19:01:21 +0200

    Update WiFi-Project.R

--- WiFi-Project.R
+++ WiFi-Project.R
@@ -1,27 +1,27 @@
 ##################################################################################################
 #                                                                                                #
-#     								Project Description                                          # 
+#     				    Project Description                                          # 
 #                                                                                                #
 #           Many real world applications need to know the localization of a user in the world to # 
 #           provide their services. So, automatic user localization has been a hot research topic# 
-#			in the last years.																	 #	
-#																								 #			
-#			The goal of the project is to build an machine learning model  which will be able    #
+#	    in the last years.		                                                         #															 #	
+#												 #												 #			
+#	    The goal of the project is to build an machine learning model  which will be able    #
 #           to predict the location of an user inside the university Universitat Jaume, Spain.   # 
 #           For that, the model will use RSSI signal strength received by mobile handsets of the #
-#           users in 3 buildings. 															     #		
-#           Total number of attributes : 529 													 #
+#           users in 3 buildings. 	                                                         #														     #		
+#           Total number of attributes : 529 							 #						 #
 #           1st 520 attributes are RSSI signal intensity. Rests are Longitude, Latitude, Floor,  #
 #           Building ID, Space ID etc.                                                           #
-#                                                    											 #	
+#                                                    						 #					 #	
 #           This piece of code (1st module) will load the data and perform preprocessing.        #                                                                                      
-#																								 #
+#												 #												 #
 #           Version: 1.0                                                                         #
-#          																						 #
-#		    Author : Ghazaleh Zamani															 #
-#																								 #
-#           Date:    12/10/2018 																 #
-#																								 #
+#          											 #											 #
+#           Author : Ghazaleh Zamani	                            				 #										 #
+#												 #												 #
+#           Date:    12/10/2018 							         #									 #
+#												 #												 #
 ##################################################################################################											           
 
 #CODE STRUCTURE###
